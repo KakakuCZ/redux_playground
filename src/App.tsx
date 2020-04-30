e0ace873724bdf4c991014216dfe724b0947dc40
Michael Kufner <>
2020-04-30 22:23:18 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import {Header} from "./components/header/Header";
+import {HomeScreen} from "./components/screens/HomeScreen";
+import {TodoListScreen} from "./components/screens/TodoListScreen";
+import {AddTodoScreen} from "./components/screens/AddTodoScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+          <Router>
+              <Header>
+                <ul className={"navigation"}>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/todoList"}>List TODOs</Link></li>
+                    <li><Link to={"/addTodo"}>Add TODO</Link></li>
+                </ul>
+              </Header>
+
+              <Switch>
+                  <Route path={"/todoList"}>
+                      <TodoListScreen/>
+                  </Route>
+
+                  <Route path={"/addTodo"}>
+                      <AddTodoScreen/>
+                  </Route>
+
+                  <Route path={"/"}>
+                      <HomeScreen />
+                  </Route>
+              </Switch>
+          </Router>
     </div>
   );
 }
